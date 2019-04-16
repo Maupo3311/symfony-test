7056609533d57cf5e0fee8470adcb93c0619bc67
Andrey Zaytsev <andrey.zaytsev@sibers.com>
2019-04-16 14:48:46 +0700

    Fixed code style. Please bring the remaining files to a similar style.

--- src/AppBundle/Controller/CategoryController.php
+++ src/AppBundle/Controller/CategoryController.php
@@ -1,6 +1,5 @@
 <?php
 
-
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\Category;
@@ -9,7 +8,6 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use AppBundle\Repository\CategoryRepository;
 use Symfony\Component\HttpFoundation\Request;
-use Doctrine\ORM\NonUniqueResultException;
 
 /**
  * Class CategoryController
@@ -19,27 +17,31 @@ use Doctrine\ORM\NonUniqueResultException;
 class CategoryController extends Controller
 {
     /**
+     * List of categories.
+     *
      * @Route("/", name="category_show")
      * @param Request $request
      * @return Response
-     * @throws NonUniqueResultException
      */
-    public function indexAction(Request $request)
+    public function indexAction(Request $request) :Response
     {
         /** @var CategoryRepository $categoryRepository */
         $categoryRepository = $this
             ->getDoctrine()
             ->getRepository(Category::class);
 
-        $page = ($request->get('page')) ? $request->get('page') : 1;
+        $page               = $request->get('page') ?: 1;
         $theNumberOnThePage = 10;
-        $quantityOfAllCategories = $categoryRepository->getTheQuantityOfAllCategories();
-        $numberOfPages = ceil($quantityOfAllCategories / $theNumberOnThePage);
-        $categories = $categoryRepository->findByPage($page, $theNumberOnThePage);
+        $countCategories    = $categoryRepository->count([]);
+        $numberOfPages      = ceil($countCategories / $theNumberOnThePage);
+        $categories         = $categoryRepository->findByPage($page, $theNumberOnThePage);
 
-        $service = $this->container->get('app.pagination');
+        $service  = $this->container->get('app.pagination');
         $position = $service->getHrefPosition($page, $numberOfPages);
 
-        return $this->render('category/index.html.twig', compact('categories', 'page', 'theNumberOnThePage', 'quantityOfAllCategories', 'numberOfPages', 'position'));
+        return $this->render(
+            'category/index.html.twig',
+            compact('categories', 'page', 'theNumberOnThePage', 'countCategories', 'numberOfPages', 'position')
+        );
     }
 }
