626d686aa586eb102dcd70c2c465e0a9d26e2646
sergey <maupo3311@mail.ru>
2019-04-16 14:06:05 +0700

    Working with admin panel

---
+++ src/AppBundle/Form/ProfileFormType.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class ProfileFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstName')
+            ->add('lastName');
+    }
+
+    public function getParent()
+    {
+        return 'FOS\UserBundle\Form\Type\ProfileFormType';
+
+    }
+
+    public function getBlockPrefix()
+    {
+        return 'app_user_profile';
+    }
+
+    public function getName()
+    {
+        return $this->getBlockPrefix();
+    }
+}
