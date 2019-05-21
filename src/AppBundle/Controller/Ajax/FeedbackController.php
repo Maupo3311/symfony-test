13cc032b7fefedb39f36c8019cc90d3492b2a904
sergey <maupo3311@mail.ru>
2019-05-21 23:47:29 +0700

    Work with ajax

--- src/AppBundle/Controller/Ajax/FeedbackController.php
+++ src/AppBundle/Controller/Ajax/FeedbackController.php
@@ -4,6 +4,7 @@ namespace AppBundle\Controller\Ajax;
 
 use AppBundle\Entity\Feedback;
 use AppBundle\Form\FeedbackType;
+use AppBundle\Repository\FeedbackRepository;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\OptimisticLockException;
 use Doctrine\ORM\ORMException;
@@ -11,6 +12,7 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use FOS\RestBundle\Controller\Annotations as Rest;
 
 /**
  * Class FeedbackController
@@ -21,34 +23,49 @@ class FeedbackController extends BaseController
 {
     /**
      * @Route("/feedback/submit", name="submit_feedback")
+     * @param Request $request
+     * @return Response
+     *
+     * @throws ORMException
+     * @throws OptimisticLockException
      */
     public function submitFeedbackAction(Request $request)
     {
         $data = $request->request->get('appbundle_feedback');
 
-        $form = $this->createForm(FeedbackType::class);
-        $form->handleRequest($request);
-        $feedback = $form->getData();
+        if (!$message = $data['message']) {
+            return $this->errorResponse('Message cannot be empty', 400);
+        } else if (!$user = $this->getUser()) {
+            return $this->errorResponse('You are not logged in', 401);
+        }
 
+        /** @var EntityManager $em */
+        $em = $this->getDoctrine()->getManager();
 
-//        if (!$message = $request->get('message')) {
-//            return $this->errorResponse('Message cannot be empty', 400);
-//        } else if (!$user = $this->getUser()) {
-//            return $this->errorResponse('You are not logged in', 401);
-//        }
-//
-//        /** @var EntityManager $em */
-//        $em = $this->getDoctrine()->getManager();
-//
-//        $feedback = new Feedback();
-//        $feedback->setUser($user)
-//            ->setMessage($message);
-//
-//        $em->persist($feedback);
-//        $em->flush();
-//
-//        return $this->successResponse('Feedback successfully sent');
+        $feedback = new Feedback();
+        $feedback->setUser($user)
+            ->setMessage($message);
 
-        return $this->successResponse($data['message']);
+        $em->persist($feedback);
+        $em->flush();
+
+        return $this->successResponse('Feedback successfully sent');
+    }
+
+    /**
+     * @Rest\Get("/feedback/load", name="load_feedback")
+     * @param Request $request
+     * @return mixed
+     */
+    public function loadFeedbackAction(Request $request)
+    {
+        $page = ($request->get('page')) ?: 1;
+        $theNumberOnThePage = ($request->get('theNumberOnThePage')) ?: 5;
+
+        /** @var FeedbackRepository $feedbackRepository */
+        $feedbackRepository = $this->getDoctrine()->getRepository(Feedback::class);
+
+        dump($feedbackRepository->findByPage($page, $theNumberOnThePage));
+        return $this->successResponse(\GuzzleHttp\json_encode($feedbackRepository->findByPage($page, $theNumberOnThePage)));
     }
 }
--- web/js/ajax/feedback.js
+++ web/js/ajax/feedback.js
@@ -1,5 +1,4 @@
-function formChecker()
-{
+function formChecker(page = 1, theNumberOnThePage = 5, userId = null) {
     $('body').on('submit', '.ajaxForm', function (e) {
 
         e.preventDefault();
@@ -8,9 +7,57 @@ function formChecker()
             type: $(this).attr('method'),
             url: $(this).attr('action'),
             data: $(this).serialize(),
-            success: function(response){
-                alert(response);
+            success: function (response) {
+                loadFeedback(page, theNumberOnThePage, userId)
             }
         })
     });
+}
+
+function loadFeedback(page, theNumberOnThePage, userId) {
+
+    $.ajax({
+        type: 'GET',
+        url: '/api/feedback-list',
+        data: 'page=' + page + '&limit=' + theNumberOnThePage,
+        success: function (feedbackArray) {
+            console.log(feedbackArray);
+            var html = '';
+
+            for (let count = 0; count < feedbackArray.length; ++count) {
+                var feedback = feedbackArray[count];
+                html += '<div class="feedback">\n' +
+                    '       <div class="feedbackHead">\n' +
+                            '    <div class="feedbackCreated">' + feedback.created + '</div>\n' +
+                            '       <p class="feedbackName">' + feedback.user.first_name + '' + feedback.user.last_name + '</p>\n' +
+                            '       <p class="feedbackEmail">' + feedback.user.email + '</p>\n' +
+                            '    </div>\n' +
+                            '    <div class="feedbackBody">\n' +
+                            '        <p class="feedbackMessage">' + feedback.message + '</p>\n';
+
+                var images = feedback.images;
+                for (let i = 0; i < images.length; ++i) {
+                    var image = images[i];
+
+                    console.log(images);
+                    html += '<div class="feedbackFile">\n' +
+                        '        <img alt="img" class="feedbackFileImages" src="uploads/images/' + image.image_path + '">\n' +
+                        '    </div>\n';
+                }
+
+                if (userId != null && feedback.user.id == userId) {
+                    html += '<div class="feedbackOptions">\n' +
+                        '        <a href="/feedback/delete/' + feedback.id + '" class="noneDecoration">\n' +
+                        '            <span class="feedbackDelete"><i class="fas fa-trash-alt"></i></span>\n' +
+                        '        </a>\n' +
+                        '    </div>'
+                }
+
+                html +=  '</div>\n' +
+                '     </div>';
+            }
+
+            $('#feedback-window').html(html);
+        }
+    })
 }
